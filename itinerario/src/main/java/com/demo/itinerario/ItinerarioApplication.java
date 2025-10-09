2f54a238b61cd23d896a2d6b7bc3d2ceb8a38ec0
SebastianCA19 <castroarrietasebastiandavid@gmail.com>
2025-10-08 19:38:05 -0500

    Creating the project

---
+++ itinerario/src/main/java/com/demo/itinerario/ItinerarioApplication.java
@@ -0,0 +1,13 @@
+package com.demo.itinerario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItinerarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItinerarioApplication.class, args);
+	}
+
+}
---
+++ itinerario/src/test/java/com/demo/itinerario/ItinerarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.itinerario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItinerarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
